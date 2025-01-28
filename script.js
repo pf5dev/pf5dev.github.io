f08a95d81d1e4e30cbc782da2a8ff9a7274d455c
pf5dev <pauloafonseca14@outlook.com>
2025-01-28 16:46:56 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+document.getElementsByClassName("contato")[0].onclick=function(){this.innerHTML="<mark>pauloafonseca14@outlook.com / 11 98190-5876</mark>";}
+document.getElementsByClassName("contato")[0].style.cursor = "pointer"});
